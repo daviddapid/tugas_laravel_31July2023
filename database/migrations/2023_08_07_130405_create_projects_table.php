5e7169fe3e8fe1bcbedaed58e81e418b0e7bf5d6
daviddapid <daviddapid@gmail.com>
2023-08-07 20:18:35 +0700

    add migrations and model (Siswa, Project, Contact, ContactType)

---
+++ database/migrations/2023_08_07_130405_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Siswa;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('siswa_id');
+            $table->foreign('siswa_id')->references('id')->on('siswas');
+            $table->string('project_name');
+            $table->date('project_date');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
